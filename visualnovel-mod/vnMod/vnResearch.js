05b0adee687a5d53feeff6ffa87f26c7459c38b3
Daniel Luque <danielluque14@gmail.com>
2013-12-07 04:26:13 +0100

    Adds new research: "Auto Save".

--- visualnovel-mod/vnMod/vnResearch.js
+++ visualnovel-mod/vnMod/vnResearch.js
@@ -2,7 +2,19 @@ var vnResearch = {};
 
 (function () {
     
-    vnResearch.addResearch = function () {        
+    vnResearch.addResearch = function () {
+        //Add Auto Save
+        GDT.addResearchItem(
+            {
+                id: "Auto Save",
+                name: "Auto Save".localize(),
+                v: 2,
+                canResearch: function (company) {
+                    return LevelCalculator.getMissionLevel('Engine') > 2;
+                },
+                category: "Engine",
+                categoryDisplayName: "Engine".localize()
+            });
     };
     
 })();
