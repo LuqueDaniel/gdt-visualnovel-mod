655e884f3c1e3f006a2214f7bc6f0d44496516d8
Daniel Luque <danielluque14@gmail.com>
2014-08-13 13:51:10 +0200

    Adjusted some values

--- visualnovel-mod/vnMod/vnTopics.js
+++ visualnovel-mod/vnMod/vnTopics.js
@@ -15,7 +15,7 @@ var vnTopics = {};
             {
                 id: "Dating Sim",
                 name: "Dating Sim".localize("game topic"),
-                genreWeightings: [0.6, 0.7, 0.8, 1.0, 0.6, 0.9],	
+                genreWeightings: [0.6, 0.7, 0.8, 1.0, 0.6, 0.8],	
                 audienceWeightings: [0.6, 0.8, 0.9],
                 missionOverrides: [
                     [0.7, 0.8, 1, 1, 0.8, 0.7, 0.9, 1, 0.8],
@@ -29,21 +29,21 @@ var vnTopics = {};
             {
                 id: "Friendship",
                 name: "Friendship".localize("game topic"),
-                genreWeightings: [0.8, 0.8, 0.8, 0.6, 0.6, 0.9],	
+                genreWeightings: [0.7, 0.8, 0.9, 0.6, 0.6, 0.9],	
                 audienceWeightings: [0.8, 0.9, 1]
             },
             
             {
                 id: "Gore",
                 name: "Gore".localize("game topic"),
-                genreWeightings: [0.9, 0.8, 0.8, 0.6, 0.6, 0.7],	
+                genreWeightings: [1.0, 0.8, 0.8, 0.6, 0.6, 0.7],	
                 audienceWeightings: [0.6, 0.6, 1]
             },
             
             {
                 id: "Kinetic Novel",
                 name: "Kinetic Novel".localize("game topic"),
-                genreWeightings: [0.6, 0.9, 0.6, 0.6, 0.6, 0.8],	
+                genreWeightings: [0.6, 0.8, 0.6, 0.6, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.8, 0.9],
                 missionOverrides: [
                     [0.6, 0.6, 1, 1, 0.8, 0.6, 0.9, 1, 0.8],
@@ -64,22 +64,22 @@ var vnTopics = {};
             {
                 id: "Romantic Comedy",
                 name: "Romantic Comedy".localize("game topic"),
-                genreWeightings: [0.7, 0.8, 0.8, 0.7, 0.6, 0.9],	
+                genreWeightings: [0.7, 0.8, 0.7, 0.7, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.9, 1]
             },
             
             {
                 id: "Survival",
                 name: "Survival".localize("game topic"),
-                genreWeightings: [0.9, 1, 0.9, 0.7, 0.9, 0.7],	
-                audienceWeightings: [0.8, 1, 0.9]
+                genreWeightings: [0.9, 1, 0.9, 0.7, 0.8, 0.8],	
+                audienceWeightings: [0.8, 1, 1]
             },
             
             {
                 id: "Science",
                 name: "Science".localize("game topic"),
                 genreWeightings: [0.7, 0.8, 0.8, 0.8, 0.8, 0.8],	
-                audienceWeightings: [0.7, 1, 1]
+                audienceWeightings: [0.7, 0.8, 1]
             }
             
         ]);
