51fc34bee544a13f91c8e3b278142c6ee6124699
Daniel Luque <danielluque14@gmail.com>
2014-08-14 12:21:34 +0200

    Replace all topics id

--- visualnovel-mod/vnMod/vnTopics.js
+++ visualnovel-mod/vnMod/vnTopics.js
@@ -6,14 +6,14 @@ var vnTopics = {};
     vnTopics.addTopics = function () {
         GDT.addTopics([
             {
-                id: "Drama",
+                id: "vnMod/Drama",
                 name: "Drama".localize("game topic"),
                 genreWeightings: [0.8, 0.8, 0.8, 0.6, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.9, 1]
             },
             
             {
-                id: "Dating Sim",
+                id: "vnMod/Dating Sim",
                 name: "Dating Sim".localize("game topic"),
                 genreWeightings: [0.6, 0.7, 0.8, 1.0, 0.6, 0.8],	
                 audienceWeightings: [0.6, 0.8, 0.9],
@@ -27,28 +27,28 @@ var vnTopics = {};
             },
             
             {
-                id: "Erogame",
+                id: "vnMod/Erogame",
                 name: "Erogame".localize("game topic"),
                 genreWeightings: [0.6, 0.8, 0.8, 0.8, 0.6, 0.6],
                 audienceWeightings: [0.6, 0.6, 1]
             },
             
             {
-                id: "Friendship",
+                id: "vnMod/Friendship",
                 name: "Friendship".localize("game topic"),
                 genreWeightings: [0.7, 0.8, 0.9, 0.6, 0.6, 0.9],	
                 audienceWeightings: [0.8, 0.9, 1]
             },
             
             {
-                id: "Gore",
+                id: "vnMod/Gore",
                 name: "Gore".localize("game topic"),
                 genreWeightings: [1.0, 0.8, 0.8, 0.6, 0.6, 0.7],	
                 audienceWeightings: [0.6, 0.6, 1]
             },
             
             {
-                id: "Kinetic Novel",
+                id: "vnMod/Kinetic Novel",
                 name: "Kinetic Novel".localize("game topic"),
                 genreWeightings: [0.6, 0.8, 0.6, 0.6, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.8, 0.9],
@@ -62,35 +62,35 @@ var vnTopics = {};
             },
             
             {
-                id: "Love",
+                id: "vnMod/Love",
                 name: "Love".localize("game topic"),
                 genreWeightings: [0.7, 0.8, 0.8, 0.8, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.9, 1]
             },
             
             {
-                id: "Magic",
+                id: "vnMod/Magic",
                 name: "Magic".localize("game topic"),
                 genreWeightings: [1, 1, 1, 0.7, 0.7, 0.9],
                 audienceWeightings: [0.8, 0.9, 0.9]
             },
             
             {
-                id: "Romantic Comedy",
+                id: "vnMod/Romantic Comedy",
                 name: "Romantic Comedy".localize("game topic"),
                 genreWeightings: [0.7, 0.8, 0.7, 0.7, 0.6, 0.9],	
                 audienceWeightings: [0.6, 0.9, 1]
             },
             
             {
-                id: "Survival",
+                id: "vnMod/Survival",
                 name: "Survival".localize("game topic"),
                 genreWeightings: [0.9, 1, 0.9, 0.7, 0.8, 0.8],	
                 audienceWeightings: [0.8, 1, 1]
             },
             
             {
-                id: "Science",
+                id: "vnMod/Science",
                 name: "Science".localize("game topic"),
                 genreWeightings: [0.7, 0.8, 0.8, 0.8, 0.8, 0.8],	
                 audienceWeightings: [0.7, 0.8, 1]
